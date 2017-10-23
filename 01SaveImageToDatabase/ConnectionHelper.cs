829d22e9c1972a924be95e97ee097bad42ea2abd
Solodye <xiblade@163.com>
2017-10-23 11:11:57 +0800

    测试程序集

---
+++ 01SaveImageToDatabase/ConnectionHelper.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+
+namespace _01SaveImageToDatabase
+{
+    public class ConnectionHelper
+    {
+        public static string GetConnectionStr()
+        {
+            return ConfigurationManager.AppSettings["ITI_TouchDB"];
+        }
+        
+    }
+}
